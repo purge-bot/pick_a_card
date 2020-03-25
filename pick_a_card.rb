b35569fddbf2d79a37d653a77c1489e131cf3648
NerovnovAA <nerovnovaa@gmail.com>
2020-03-25 16:10:01 +0300

    symbol

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 # Проверка
 
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suits = %w[Diamonds Hearts Clubs Spades]
+suits = ["\u2666", "\u2665", "\u2663", "\u2660"]
 
 puts "#{values.sample} of #{suits.sample}"
